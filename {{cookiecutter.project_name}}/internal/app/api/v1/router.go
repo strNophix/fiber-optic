d49ca4fa68afcfaa8896e2b8cade40c791c699c1
niku <nvdpoel01@gmail.com>
2023-07-22 14:31:55 +0200

    Added request ID middleware to api/v1

--- {{cookiecutter.project_name}}/internal/app/api/v1/router.go
+++ {{cookiecutter.project_name}}/internal/app/api/v1/router.go
@@ -2,10 +2,13 @@ package v1
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 func ConfigureRoutes(router fiber.Router) error {
 	v1 := router.Group("/v1")
+
+	v1.Use(requestid.New())
 
 	v1.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendString("pong")
